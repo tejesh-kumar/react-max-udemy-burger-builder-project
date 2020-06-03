efc9d029183a309f114d75956c3d9fecf7a81610
tejesh <tejeshmuralikrishna@gmail.com>
2020-06-03 13:45:15 +0530

    Starting implementation of burger-builder component

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Auxiliary'
+
+class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            meat: 0,
+            cheese: 0,
+            salad: 0
+        }
+    }
+
+    render() {
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
+
+}
+
+export default BurgerBuilder;

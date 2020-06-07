7f1d6c2c24aa246051d32973aa16990783bf6d3b
tejesh <tejeshmuralikrishna@gmail.com>
2020-06-07 14:32:14 +0530

    Removing Ingredients safely Creating a dummy ingredient object, storing true or false based on quantity of ingredients to disable the 'remove' ingredient button if the ingredient is already zero. In render method,         const  disabledInfo = {             ...this.state.ingredients         }
            for(let type in disabledInfo) {
                disabledInfo[type] = disabledInfo[type] <= 0;
            }

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -13,7 +13,12 @@ const controls = [
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
         {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label} added={() => { props.ingredientAdded(ctrl.type) }} />
+            <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label} 
+                added={() => { props.ingredientAdded(ctrl.type) }} 
+                removed={() => { props.ingredientRemoved(ctrl.type) }} 
+                disabled={ props.disabledInfo[ctrl.type] } />
         ))}
     </div>
 )

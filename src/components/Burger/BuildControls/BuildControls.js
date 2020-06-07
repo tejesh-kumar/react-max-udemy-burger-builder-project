b1da8899c36bdc8c9b445b480014fb56d5a9dd0c
tejesh <tejeshmuralikrishna@gmail.com>
2020-06-07 13:39:31 +0530

    Connecting state to build controls for ingredient addition

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -8,13 +8,12 @@ const controls = [
     { label: 'Bacon', type: 'bacon' },
     { label: 'Cheese', type: 'cheese' },
     { label: 'Meat', type: 'meat' }
-
 ]
 
 const buildControls = (props) => (
     <div className={classes.BuildControls}>
         {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label} />
+            <BuildControl key={ctrl.label} label={ctrl.label} added={() => { props.ingredientAdded(ctrl.type) }} />
         ))}
     </div>
 )

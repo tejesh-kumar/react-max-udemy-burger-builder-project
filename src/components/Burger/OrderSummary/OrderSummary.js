9357a2781de3a74b1ce47d98b1c9c021695c9ea2
tejesh <tejeshmuralikrishna@gmail.com>
2020-06-16 01:32:43 +0530

    Performance improvements using lifecycle methods in Modal. Scenario: If Modal is closed, update to OrderSummary on state change is not required. hence, using shouldComponentUpdate() in Modal to check condition for updating & re-render. Now, Modal is not re-rendered if it is closed. Also, any other components like OrderSummary does not update if Modal does not get updated.

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    componentDidUpdate() {
+        console.log('[OrderSummary] WillUpdate!');
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map((igKey) => {
             return (
                 <li key={igKey}>
-                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
                 </li>
             )
         })
+ 
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with following ingredients: </p>
+                { ingredientSummary }
+                <p><strong>Total Price: <span>{this.props.price.toFixed(2)}</span></strong></p>
+    
+                <p>Continue to checkout?</p>
+                <Button clicked={this.props.purchaseCancelled} btnType={'Danger'}>CANCEL</Button>
+                <Button clicked={this.props.purchaseContinued} btnType={'Success'}>CONTINUE</Button>
+            </Aux>
+        )
+    }
+} 
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with following ingredients: </p>
-            { ingredientSummary }
-            <p><strong>Total Price: <span>{props.price.toFixed(2)}</span></strong></p>
-
-            <p>Continue to checkout?</p>
-            <Button clicked={props.purchaseCancelled} btnType={'Danger'}>CANCEL</Button>
-            <Button clicked={props.purchaseContinued} btnType={'Success'}>CONTINUE</Button>
-        </Aux>
-    )
-}
-
-export default orderSummary;
+export default OrderSummary;

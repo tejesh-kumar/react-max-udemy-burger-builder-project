3b51fd46ce8bfc5ccfe78b2e2b3865dd28b4b4f8
tejesh <tejeshmuralikrishna@gmail.com>
2020-07-08 18:36:32 +0530

    Passing Ingredients via queryParams

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -12,6 +12,16 @@ class Checkout extends Component {
         }
     }
 
+    componentDidMount () {
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+        for (let param of query.entries()) {
+            // ['bacon', '1']
+            ingredients[param[0]] = +param[1];
+        }
+        this.setState({ingredients: ingredients});
+    }
+
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
     }
@@ -21,6 +31,8 @@ class Checkout extends Component {
     }
 
     render () {
+
+
         return (
             <div>
                 <CheckoutSummary ingredients={this.state.ingredients} onCheckoutCancelled={this.checkoutCancelledHandler} onCheckoutContinued={this.checkoutContinuedHandler}  />

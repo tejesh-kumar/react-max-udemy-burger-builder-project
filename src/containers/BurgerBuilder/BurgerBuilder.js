86565b79c0389fad573bee51704732dc449e6ed8
tejesh <tejeshmuralikrishna@gmail.com>
2020-07-08 16:44:14 +0530

    Navigating to checkout page On clicking continue in the modal, we go to checkout page by adding the push() in the purchaseContinueHandler in BurgerBuilder.     this.props.history.push('/checkout');
    The direct components like 'Checkout', 'BurgerBuilder' loaded through route of react-router-dom has access to match, location, history props.
    The other components can access these properties using { withRouter } of react-router-dom => Wrapping the component with 'WithRouter' at the export.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -101,31 +101,33 @@ class BurgerBuilder extends Component {
     purchaseContinueHandler = () => {
         // alert('You continue!'); 
 
-        this.setState({loading: true})
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Tejesh',
-                address: {
-                    street: 'SCB road',
-                    zipCode: 560070,
-                    country: 'India'
-                },
-                email: 'tejesh@gmail.com'
-            },
-            deliveryMethod: 'fastest'
-        }
+        // this.setState({loading: true})
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Tejesh',
+        //         address: {
+        //             street: 'SCB road',
+        //             zipCode: 560070,
+        //             country: 'India'
+        //         },
+        //         email: 'tejesh@gmail.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
 
-        axios.post('/orders.json', order)
-            .then(response => {
-                console.log(response);
-                this.setState({loading: false, purchasing: false})
-            })
-            .catch(error => {
-                this.setState({loading: false, purchasing: false})
-                console.log(error);
-            });
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         console.log(response);
+        //         this.setState({loading: false, purchasing: false})
+        //     })
+        //     .catch(error => {
+        //         this.setState({loading: false, purchasing: false})
+        //         console.log(error);
+        //     });
+
+        this.props.history.push('/checkout');
     }
 
     render() {

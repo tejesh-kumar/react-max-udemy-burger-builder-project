213fb16313f17eaca7dd49f96cfd881f1631edc8
tejesh <tejeshmuralikrishna@gmail.com>
2020-08-08 18:00:54 +0530

    Order Submission & passing data between pages Adding orderHandler on order button click in checkout.     <Route path={this.props.match.path + '/contact-data'} component={ContactData} /> Instead of above 'component' attribute we can use 'render' to render ContactData component along with ingredient information that can be passed.
    componentDidMount() is replaced by componentWillMount().
    
        <Route
        path={this.props.match.path + '/contact-data'}
        render={(props) => (<ContactData ingredients={this.state.ingredients} price={this.state.totalPrice} {...props} />)} />
    
    {...props} is used to pass the history object through props to contactData component since we have rendered the contactData component manually using the render method, the 'match,history' will not be available by default inside the component.
    
    Loading a spinner until submitting the form in contactData component.
    Redirect the user once form is submitted to '/'

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -103,36 +103,11 @@ class BurgerBuilder extends Component {
     purchaseContinueHandler = () => {
         // alert('You continue!'); 
 
-        // this.setState({loading: true})
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        //     customer: {
-        //         name: 'Tejesh',
-        //         address: {
-        //             street: 'SCB road',
-        //             zipCode: 560070,
-        //             country: 'India'
-        //         },
-        //         email: 'tejesh@gmail.com'
-        //     },
-        //     deliveryMethod: 'fastest'
-        // }
-
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         console.log(response);
-        //         this.setState({loading: false, purchasing: false})
-        //     })
-        //     .catch(error => {
-        //         this.setState({loading: false, purchasing: false})
-        //         console.log(error);
-        //     });
-
         const queryParams = [];
         for(let i in this.state.ingredients) {
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice);
         const queryString = queryParams.join('&');
 
         this.props.history.push({
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 
+import axios from '../../../axios-orders'
 import Button from '../../../components/UI/Button/Button'
+import Spinner from '../../../components/UI/Spinner/Spinner'
 import classes from './ContactData.css'
 
 export class ContactData extends Component {
@@ -10,20 +12,61 @@ export class ContactData extends Component {
         address: {
             street: '',
             postalCode: ''
+        },
+        loading: false
+    }
+
+    orderHandler = (e) => {            
+        e.preventDefault();
+        // console.log(this.props.ingredients);
+
+        this.setState({loading: true})
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer: {
+                name: 'Tejesh',
+                address: {
+                    street: 'SCB road',
+                    zipCode: 560070,
+                    country: 'India'
+                },
+                email: 'tejesh@gmail.com'
+            },
+            deliveryMethod: 'fastest'
         }
+
+        axios.post('/orders.json', order)
+            .then(response => {
+                console.log(response);
+                this.setState({loading: false})
+                this.props.history.push('/');           // Redirect the user once form is submitted
+            })
+            .catch(error => {
+                this.setState({loading: false})
+                console.log(error);
+            });
     }
 
     render() {
+        let form = (
+            <form>
+                <input className={classes.Input} type="text" name="name" placeholder="Your name" />
+                <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
+                <input className={classes.Input} type="text" name="street" placeholder="Street" />
+                <input className={classes.Input} type="number" name="postal" placeholder="Postal Code" />
+                <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+            </form>
+        );
+
+        if(this.state.loading) {
+            form = <Spinner />
+        }
+
         return (
             <div className={classes.ContactData} >
                 <h4>Enter your contact details</h4>
-                <form>
-                    <input className={classes.Input} type="text" name="name" placeholder="Your name" />
-                    <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
-                    <input className={classes.Input} type="text" name="street" placeholder="Street" />
-                    <input className={classes.Input} type="number" name="postal" placeholder="Postal Code" />
-                    <Button btnType="Success">ORDER</Button>
-                </form>
+                { form }
             </div>
         )
     }

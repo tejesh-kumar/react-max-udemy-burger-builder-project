7f13eb11c21f0b9b3dd029e99497e53496071aee
tejesh <tejeshmuralikrishna@gmail.com>
2020-08-09 12:55:53 +0530

    Outpuuting the orders & styling it

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -24,15 +24,18 @@ class Orders extends Component {
             })
             .catch(err => {
                 this.setState({loading: false})
-
             })
     }
 
     render () {
         return(
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order 
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={order.price} />
+                ))}
             </div>
         )
     }
